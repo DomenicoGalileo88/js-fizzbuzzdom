786072c786055eb2b46282d5f98152ffe90ee3ad
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-28 17:12:38 +0200

    Add screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,8 +12,8 @@ proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 //stampa in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
 
-    let row = document.querySelector('.row');
     //Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+    let row = document.querySelector('.row');
     let number = `<div class="col">
                       <div class="square d-flex justify-content-center align-items-center">${i}</div>
                   </div>`;
@@ -23,14 +23,25 @@ for (let i = 1; i <= 100; i++) {
     //Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz" al posto del numero
     if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log('FizzBuzz');
+        let number_5_3 = `<div class="col">
+                            <div class="square bg-danger d-flex justify-content-center align-items-center">FizzBuzz</div>
+                        </div>`;
+        row.insertAdjacentHTML('beforeend', number_5_3);   
 
         //Per i multipli di 5 stampi "Buzz" al posto del numero
     } else if (i % 5 == 0) {
         console.log('Buzz');
-
+        let number_5 = `<div class="col">
+                            <div class="square bg-success d-flex justify-content-center align-items-center">Buzz</div>
+                        </div>`;
+        row.insertAdjacentHTML('beforeend', number_5);  
         //Per i multipli di 3 stampi “Fizz” al posto del numero
     } else if (i % 3 == 0) {
         console.log('Fizz');
+        let number_3 = `<div class="col">
+                            <div class="square bg-warning d-flex justify-content-center align-items-center">Fizz</div>
+                        </div>`;
+        row.insertAdjacentHTML('beforeend', number_3);  
 
     } else {
         //stampa il valore di "i"
